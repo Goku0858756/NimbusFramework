d8b9f3c6a0d93f8f798af0979eb9cb4aade4a628
Goku <0858756@hr.nl>
2015-10-05 11:16:19 +0200

    Begin of something great

--- Crawler/controller.py
+++ Crawler/controller.py
@@ -1,1 +1,14 @@
 __author__ = 'N05F3R4TU'
+
+class Crawler(object):
+
+    def __init__(self):
+        self.name = ''
+        self.session = ''
+        self.pid = ''
+        self.state = ''
+
+
+
+if __name__ == '__main__':
+    Crawler()
