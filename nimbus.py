4a087efd377f499e9458f72e82adb6cca9b4eb7a
Goku <0858756@hr.nl>
2015-10-05 11:23:51 +0200

    Core

---
+++ nimbus.py
@@ -0,0 +1,19 @@
+__author__ = 'N05F3R4TU'
+
+# TODO: Create Basic Crawler
+# TODO: Create MasScanner
+# TODO: Create Plugins
+# TODO: Plugin: Gmail
+# TODO: Create System
+# TODO: System: [ check for ] Update
+# TODO: System: Encryption and Decryption
+# TODO: Thread Control System
+# TODO: CommandLineInterface
+
+# MOST IMPORTANT
+# TODO: Threading
+# TODO: Services
+# TODO: Modules
+# TODO: Plugins
+# TODO: Body/Framework
+# TODO: Documentation System Pythonic
